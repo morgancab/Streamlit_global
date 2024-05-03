8787a553f64b4753d4d1846dfb5806a4038438f1
mccabedo <mccabedo@amazon.lu>
2024-05-03 13:34:27 +0100

    small changes

--- pages/5_🌞_Weather_France.py
+++ pages/5_🌞_Weather_France.py
@@ -15,6 +15,8 @@ Weather = Weather[["NUM_POSTE", "NOM_USUEL", "LAT", "LON", "ALTI", "AAAAMM", "RR
 Weather['NBJ_Pluie'] = Weather['NBJRR1'] + Weather['NBJRR5']  + Weather['NBJRR10'] + Weather['NBJRR30'] + Weather['NBJRR50']
 Weather=Weather.rename(columns={"AAAAMM": "week", "RR": "Cumul_precipitation" ,"TX": "Temperature_max_AVG" , " TN": "Temperature_min_AVG"})
 Weather =Weather.drop(columns=['NBJRR1', 'NBJRR5','NBJRR10','NBJRR30','NBJRR50'])
+Weather['NOM_USUEL'] = str(Weather['NOM_USUEL'])
+
 
 #Show data
 if st.checkbox('Show raw data'):
